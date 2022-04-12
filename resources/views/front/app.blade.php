7eca561a8abb1bee3765c9306277c0117ac19287
Alex <cyrus1988@ukr.net>
2022-04-12 12:20:28 +0300

    Reworked base and home template

--- resources/views/front/app.blade.php
+++ resources/views/front/app.blade.php
@@ -1,39 +1,11 @@
 @include('front.layouts.header')
-<!--top-header-->
-<div class="top-header">
-    <div class="container">
-        <div class="top-header-main">
-            <div class="col-md-6 top-header-left">
-                <div class="drop">
-                    @include('front.layouts.components.top.currency')
-                    @include('front.layouts.components.top.language')
-                    <div class="clearfix"></div>
-                </div>
-            </div>
-            @include('front.layouts.components.top.cart')
-            <div class="clearfix"></div>
-        </div>
-    </div>
-</div>
-<!--top-header-->
-<!--start-logo-->
-@include('front.layouts.components.top.logo')
-<!--start-logo-->
-<!--bottom-header-->
-<div class="header-bottom">
-    <div class="container">
-        <div class="header">
-            @include('front.layouts.components.top.menu')
-            @include('front.layouts.components.top.search')
-            <div class="clearfix"></div>
-        </div>
-    </div>
-</div>
-
+<!--top-info-block-starts-->
+@include('front.layouts.components.top')
+<!--top-info-block-end-->
 <!--product-starts-->
 @yield('content')
 <!--product-end-->
 <!--information-starts-->
-@include('front.layouts.components.bottom.info')
+@include('front.layouts.components.bottom')
 <!--information-end-->
 @include('front.layouts.footer')
