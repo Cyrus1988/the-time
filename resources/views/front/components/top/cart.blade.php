89a98ee80b3dfd9652444c7a0af668d1e60131a6
Cyrus <Cyrus1988@ukr.net>
2022-04-27 17:21:07 +0300

    Added cart to header

---
+++ resources/views/front/components/top/cart.blade.php
@@ -0,0 +1,9 @@
+<div class="cart box_1">
+    <a href="checkout.html">
+        <div class="total">
+            <span class="simpleCart_total"></span></div>
+        <img src="{{ asset('storage/' .'cart-1.png')}}" alt=""/>
+    </a>
+    <p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+    <div class="clearfix"></div>
+</div>
